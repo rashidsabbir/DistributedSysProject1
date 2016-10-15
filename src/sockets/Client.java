f0c251158037316d03b0a6aaacf085d0a66f202a
Sabbir <rashidsabbir@gmail.com>
2016-10-15 17:04:44 -0400

    Server and Client Classes

---
+++ src/sockets/Client.java
@@ -0,0 +1,37 @@
+package sockets;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+	public static void main(String[] args) throws IOException {
+		
+		String hostName = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+
+		try {
+		    Socket clientSocket = new Socket(hostName, portNumber);
+		    PrintWriter out =
+		        new PrintWriter(clientSocket.getOutputStream(), true);
+		    BufferedReader in =
+		        new BufferedReader(
+		            new InputStreamReader(clientSocket.getInputStream()));
+		    BufferedReader stdIn =
+		        new BufferedReader(
+		            new InputStreamReader(System.in));
+		    
+		    String userInput;
+			while ((userInput = stdIn.readLine()) != null) {
+			    out.println(userInput);
+			    System.out.println("echo: " + in.readLine());
+			}
+			clientSocket.close();
+			
+		} catch (IOException e) {
+			System.err.println("Caught IOException: " + e.getMessage());
+		} finally {
+			
+		}
+				
+	}
+}
