92cb6ba4e0fa244543c878893b0ccc48171b8c3e
Sabbir <rashidsabbir@gmail.com>
2016-10-15 18:47:56 -0400

    Fixed Client Code

--- src/sockets/Client.java
+++ src/sockets/Client.java
@@ -5,38 +5,39 @@ import java.net.*;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		
-		if (args.length != 2) {
-            System.err.println("Usage: java Client <host name> <port number>");
+        
+        if (args.length != 2) {
+            System.err.println(
+                "Usage: java Client <host name> <port number>");
             System.exit(1);
         }
-		
-		String hostName = args[0];
-		int portNumber = Integer.parseInt(args[1]);
-
-		try {
-			Socket clientSocket = new Socket(hostName, portNumber);
-		    PrintWriter out =
-		        new PrintWriter(clientSocket.getOutputStream(), true);
-		    BufferedReader in =
-		        new BufferedReader(
-		            new InputStreamReader(clientSocket.getInputStream()));
-		    BufferedReader stdIn =
-		        new BufferedReader(
-		            new InputStreamReader(System.in));
-		    
-		    String userInput;
-			while ((userInput = stdIn.readLine()) != null) {
-			    out.println(userInput);
-			    System.out.println("echo: " + in.readLine());
-			}
-			clientSocket.close();
-			
-		} catch (IOException e) {
-			System.err.println("Caught IOException: " + e.getMessage());
-		} finally {
-			
-		}
-				
-	}
+ 
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+ 
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out =
+                new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in =
+                new BufferedReader(
+                    new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn =
+                new BufferedReader(
+                    new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }
 }
