6f08078d4581754f2200aa5ca80c1e03648e3d4f
Sabbir <rashidsabbir@gmail.com>
2016-10-16 12:56:23 -0400

    Working on server read

--- src/sockets/Server.java
+++ src/sockets/Server.java
@@ -64,10 +64,8 @@ public class Server {
         		}
         		else if(result.substring(0,4).equalsIgnoreCase("read"))
         		{
-        			out.println("Reading File...");
-        			out.flush();
-        			System.out.println(Main.ReadFile(result.substring(5,result.length())));
-        			out.println(Main.ReadFile(result.substring(5,result.length())));
+        			String temp = Main.ReadFile(result.substring(5,result.length()));
+        			out.println("Reading File...\n" + temp);
         			out.flush();
         		}
         		else if(result.substring(0,6).equalsIgnoreCase("append"))
